ac42e5bb309bc714b4e8ed66b2d595a05d0ab47a
DDRAGON <davide.0829@gmail.com>
2018-03-08 16:45:07 +0900

    require to import

--- app/entry.js
+++ app/entry.js
@@ -1,7 +1,7 @@
 'use strict';
-const  $ = require('jquery');
-const  block = $('#block');
-const  scalingButton = $('#scaling-button');
+import $ from 'jquery';
+const block = $('#block');
+const scalingButton = $('#scaling-button');
 
 scalingButton.click(() => {
   block.animate({ width: '200pt', height: '200pt' }, 2000);
