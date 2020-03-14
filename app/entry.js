f830b20adfd6a583666f8975f706dc32fb189a7e
Yutaka Kobayashi <made.in.yutaka@gmail.com>
2020-03-15 07:20:17 +0900

    練習問題の実装: block が動くための処理を実装

--- app/entry.js
+++ app/entry.js
@@ -2,8 +2,14 @@
 import $ from 'jquery';
 const block = $('#block');
 const scalingButton = $('#scaling-button');
+const movingButton = $('#moving-button');
 
 scalingButton.click(() => {
   block.animate({ width: '200pt', height: '200pt' }, 2000);
   block.animate({ width: '100pt', height: '100pt' }, 2000);
 });
+
+movingButton.click(() => {
+  block.animate({ marginLeft: '500px'}, 500);
+  block.animate({ marginLeft: '20px'}, 1000);
+})
